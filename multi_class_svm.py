c199ab385f60539c8064a36d6edf8cc428e75c75
izaks <iyarzaks@gmail.com>
2019-07-29 22:48:48 +0300

    change to python file order, remove prints.

--- multi_class_svm.py
+++ multi_class_svm.py
@@ -1,25 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[56]:
-
-
-
 import pandas as pd
 from sklearn.metrics import confusion_matrix 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 
-# In[57]:
-
-
 df = pd.read_csv('kc_house_data.csv')
 df = df.sample(10000)
 df.head()
 
-
-# In[58]:
 
 
 #removed_features = ['date', 'id']
@@ -29,36 +17,11 @@ df.head()
 #normalize all columns
 #price to categorial
 
-
-# In[59]:
-
-
 new_df_columns = list(set(df.columns) - set(['date', 'id','long','lat']))
 print (new_df_columns)
-
-
-# In[60]:
-
-
 update_df = df[new_df_columns]
-
-
-# In[61]:
-
-
-update_df.head()
-
-
-# In[62]:
-
-
 update_df['yr_built'] = update_df['yr_built'].apply(lambda x: 2015-x)
 update_df['yr_built']
-
-
-# In[63]:
-
-
 def year_to_bin(year):
     if year != 0:
         return 1
@@ -66,27 +29,8 @@ def year_to_bin(year):
         return 0
 
 update_df['yr_renovated'] = update_df['yr_renovated'].apply(year_to_bin)
-
-
-# In[64]:
-
-
 update_df = pd.get_dummies(update_df,columns=['zipcode'])
-
-
-# In[65]:
-
-
-update_df.head()
-
-
-# In[66]:
-
-
-x= update_df.loc[:, update_df.columns != 'price']
-
-
-# In[67]:
+x = update_df.loc[:, update_df.columns != 'price']
 
 
 def price_to_class(price):
@@ -100,46 +44,14 @@ def price_to_class(price):
         return 3
     else:
         return 4
-    
-
-
-# In[68]:
-
 
 Y = update_df['price']
 Y=Y.apply(price_to_class)
-
-
-# In[69]:
-
-
-Y
-
-
-# In[70]:
-
-
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 normalize_x=scaler.fit_transform(x)
-
-
-# In[71]:
-
-
 X = pd.DataFrame(normalize_x)
-X.head()
-
-
-# In[72]:
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state = 0) 
-
-
-# In[73]:
-
-
+X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state = 0)
 svm_model_linear = SVC(kernel = 'linear', C = 1).fit(X_train, y_train) 
 svm_predictions = svm_model_linear.predict(X_test) 
   
@@ -150,10 +62,6 @@ accuracy = svm_model_linear.score(X_test, y_test)
 cm = confusion_matrix(y_test, svm_predictions) 
 print (accuracy)
 print (cm)
-
-
-# In[33]:
-
 
 svm_model_linear.coef_[0]
 
